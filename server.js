fd2cbd94a8e4f11dfc8883dad9e9f7dc6d5d5c37
Glitch (bevel-tinted-crayfish) <none>
2020-07-30 01:00:07 +0000

    🛬🕋 Updated with Glitch

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 
 // init project
 var express = require('express');
+var moment = require('moment');
+
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -18,38 +20,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+/* your first API endpoint */
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-//
-
-
-// your 2nd API endpoint... 
-app.get("/api/whoami", function (req, res) {
-  var ipaddr = req.ip;
-  var lang = req.headers["accept-language"];
-  var user_info = req.headers["user-agent"];
-  
-  res.json({ipaddress: ipaddr,
-           language: lang,
-           software: user_info});
+/* Timestamp endpoint API. Serves current date */
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
 });
 
-
+/* Timestamp endpoint API with date_string parameter. Serves input date's unix and utc  */
+app.get("/api/timestamp/:date_string?", function(req, res){
+  req.time_iso = req.params.date_string;
+  if (moment(req.time_iso)){
+    req.unix = new Date(req.time_iso).getTime();
+    req.utc = new Date(req.time_iso).toUTCString();
+  }
+  if (/\d{5,}/.test(req.time_iso)){
+    console.log("time unix input ");
+    req.unix = req.time_iso;
+    req.utc = new Date(parseInt(req.time_iso)).toUTCString();
+    }
+  if(!req.unix || req.utc === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+  return res.json({ unix: req.unix, utc: req.utc });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-//var string = req.method + " " + req.path + " - " + req.ip;
-
-/*app.use(function middleware(req, res, next) {
-  // Do something
-  // Call the next function in line:
-  next();
-});*/
